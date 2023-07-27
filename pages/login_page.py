8c9b2bcae8ced75b24d37dd5a4cc1bad7d2c40db
LipatnikovaAV <aerovaya@gmail.com>
2023-07-27 13:03:25 +0400

    Add: login_page

---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators as locator
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "Current url is invalid."
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*locator.FORM_LOGIN), "Form login is not presented on the page."
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*locator.FORM_REGISTRATION), "Form registration is not presented on the page."
+
+
+class TestLoginPage:
+    def test_should_be_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_be_login_page()
