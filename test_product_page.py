04d13b32752eaa50b6a67f05ca2bf26fe0f61a04
LipatnikovaAV <aerovaya@gmail.com>
2023-07-29 21:25:30 +0400

    Ref: base page

--- test_product_page.py
+++ test_product_page.py
@@ -45,3 +45,15 @@ class TestProductPage:
         page.open()
         page.click_button_add_to_basket()
         page.should_be_disappearing_message()
+
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
